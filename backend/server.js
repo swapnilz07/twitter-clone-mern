5aa896282aa68a8f8766bd85a161ab5806c6aedb
swapnil <swapnil.zakade.33@gmail.com>
2024-09-08 22:17:57 +0530

    prepared for production.

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.htmml"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
